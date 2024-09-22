40bd6d4db75ea3bec1d925ff6fa203ad9fbedebb
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-22 03:55:54 +0300

    feat: add rating source entity

---
+++ src/main/java/com/software/modsen/ratingmicroservice/entities/rating/rating_source/RatingSource.java
@@ -0,0 +1,26 @@
+package com.software.modsen.ratingmicroservice.entities.rating.rating_source;
+
+import com.software.modsen.ratingmicroservice.entities.rating.Rating;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "rating_source")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RatingSource {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "rating_id")
+    private List<Rating> rating;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "source", nullable = false)
+    private Source source;
+}
---
+++ src/main/java/com/software/modsen/ratingmicroservice/entities/rating/rating_source/Source.java
@@ -0,0 +1,5 @@
+package com.software.modsen.ratingmicroservice.entities.rating.rating_source;
+
+public enum Source {
+    PASSENGER, DRIVER;
+}
