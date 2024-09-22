222cf5adee97230b70d42d1d5be16a38a46938bb
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-22 14:48:11 +0300

    feat: add rating controller

---
+++ src/main/java/com/software/modsen/ratingmicroservice/controllers/RatingController.java
@@ -0,0 +1,54 @@
+package com.software.modsen.ratingmicroservice.controllers;
+
+import com.software.modsen.ratingmicroservice.entities.rating.Rating;
+import com.software.modsen.ratingmicroservice.entities.rating.RatingDto;
+import com.software.modsen.ratingmicroservice.entities.rating.RatingPatchDto;
+import com.software.modsen.ratingmicroservice.entities.rating.rating_source.Source;
+import com.software.modsen.ratingmicroservice.services.RatingService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/rating", produces = "application/json")
+public class RatingController {
+    @Autowired
+    private RatingService ratingService;
+
+    @GetMapping
+    public ResponseEntity<List<Rating>> getAllRatings() {
+        return ResponseEntity.ok(ratingService.getAllRatings());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Rating> getRatingById(@PathVariable("id") long id) {
+        return ResponseEntity.ok(ratingService.getRatingById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Rating> saveRating(@RequestParam Source ratingSource,
+                                             @Valid RatingDto ratingDto) {
+        return ResponseEntity.ok(ratingService.saveRating(ratingSource, ratingDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Rating> updateRatingById(@PathVariable("id") long id,
+                                                   @Valid @RequestBody RatingDto ratingDto) {
+        return ResponseEntity.ok(ratingService.updateRating(id, ratingDto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Rating> patchRatingById(@PathVariable("id") long id,
+                                                  @Valid @RequestBody RatingPatchDto ratingPatchDto) {
+        return ResponseEntity.ok(ratingService.patchRating(id, ratingPatchDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteRatingById(@PathVariable("id") long id) {
+        ratingService.deleteRatingById(id);
+        return ResponseEntity.ok("Rating was successfully deleted by id " + id);
+    }
+}
