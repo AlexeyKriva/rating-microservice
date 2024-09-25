2ba41673271607edf513bbce9fd1b9b11aa17768
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-25 17:44:38 +0300

    chore: init eureka client

--- src/main/java/com/software/modsen/ratingmicroservice/RatingMicroserviceApplication.java
+++ src/main/java/com/software/modsen/ratingmicroservice/RatingMicroserviceApplication.java
@@ -2,12 +2,14 @@ package com.software.modsen.ratingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableFeignClients(basePackages = "com.software.modsen.ratingmicroservice.clients")
+@EnableDiscoveryClient
 public class RatingMicroserviceApplication {
 
     public static void main(String[] args) {
