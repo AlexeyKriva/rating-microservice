ac96f3f013a1287e29d2fd88d7e321f66b27a393
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-10-03 23:53:00 +0300

    chore: update kafka config

--- src/main/java/com/software/modsen/ratingmicroservice/configs/kafka/KafkaProducerConfig.java
+++ src/main/java/com/software/modsen/ratingmicroservice/configs/kafka/KafkaProducerConfig.java
@@ -2,37 +2,33 @@ package com.software.modsen.ratingmicroservice.configs.kafka;
 
 import com.software.modsen.ratingmicroservice.entities.driver.DriverRatingMessage;
 import com.software.modsen.ratingmicroservice.entities.passenger.PassengerRatingMessage;
+import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.core.env.Environment;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@AllArgsConstructor
 public class KafkaProducerConfig {
-    @Value("${spring.kafka.producer.bootstrap-servers}")
-    private String bootstrapAddress;
-
-    @Value("${spring.kafka.producer.key-serializer}")
-    private String keySerialize;
-
-    @Value("${spring.kafka.producer.value-serializer}")
-    private String valueSerializer;
-
-    @Value("${spring.kafka.producer.properties.enable.idempotence}")
-    private String enableIdempotence;
+    private Environment environment;
 
     public Map<String, Object> producerFactory() {
         Map<String, Object> kafkaProducerProps = new HashMap<>();
-        kafkaProducerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        kafkaProducerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerialize);
-        kafkaProducerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
-        kafkaProducerProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);
+        kafkaProducerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                environment.getProperty("spring.kafka.producer.bootstrap-servers"));
+        kafkaProducerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        kafkaProducerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        //kafkaProducerProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);
 
         return kafkaProducerProps;
     }
